b47c60a25732d2536e8bf1e71142bba88c972e6f
Marijn Ophorst <marijn@monomelodies.nl>
2017-04-26 07:19:27 +0200

    fix trigger plugin for mysql; was a bit outdated

--- src/Plugin.php
+++ src/Plugin.php
@@ -13,23 +13,30 @@ use PDO;
 
 class Plugin extends Core\Plugin
 {
+    public $description = 'Dropping existing triggers...';
+
     public function __invoke(string $sql) : string
     {
         $stmt = $this->loader->getPdo()->prepare(
             "SELECT trigger_name, event_object_table
-                FROM INFORMATION_SCHEMA.TRIGGERS WHERE
-                ((TRIGGER_CATALOG = ? AND TRIGGER_SCHEMA = 'public') OR TRIGGER_SCHEMA = ?)");
-        $stmt->execute([$this->loader->getDatabase(), $this->loader->getDatabase()]);
+                FROM INFORMATION_SCHEMA.TRIGGERS WHERE TRIGGER_SCHEMA = ?");
+        $stmt->execute([$this->loader->getDatabase()]);
         foreach ($stmt->fetchAll(PDO::FETCH_ASSOC) as $trigger) {
-            $this->loader->addOperation("DROP TRIGGER {$trigger['trigger_name']};");
+            $this->addOperation("DROP TRIGGER {$trigger['trigger_name']};");
         }
         if (preg_match_all("@^CREATE TRIGGER.*?^END;$@ms", $sql, $triggers, PREG_SET_ORDER)) {
             foreach ($triggers as $trigger) {
-                $this->triggers[] = $trigger[0];
+                $this->defer($trigger[0]);
                 $sql = str_replace($trigger[0], '', $sql);
             }
         }
         return $sql;
+    }
+
+    public function __destruct()
+    {
+        $this->description = 'Recreating triggers...';
+        parent::__destruct();
     }
 }
 
